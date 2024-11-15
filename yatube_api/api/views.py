cab3ae0ef03ece07132f57c1ac5f0c945ed673ed
Кутузов Ринат <rust0267@gmail.com>
2024-11-15 21:38:03 +1000

    ready proj

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from posts.models import (Post,
                           Group,
                           Comment)
-from rest_framework import viewsets, status, mixins
+from rest_framework import viewsets, status
 from .serializers import (PostSerializer,
                           GroupSerializer, CommentSerializer)
 from django.contrib.auth import get_user_model
